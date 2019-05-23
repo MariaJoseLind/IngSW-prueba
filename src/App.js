12c4b7955eae6adf526899cbbbe058b1bd30dcee
Maria Lind <daniela.lind@globant.com>
2019-05-23 18:08:09 -0300

    cosa 9

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div>Majo majo majo</div>
+        <div>cosa de mierda</div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
