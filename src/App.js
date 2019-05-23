8298083029dde4d2357a04d01fd15c13bd701348
Maria Lind <daniela.lind@globant.com>
2019-05-23 18:35:48 -0300

    cosa 15

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <div>HOLA</div>
         <div>
-          ojala algun dia ande esto
+          ojala algun dia ande esto UIOUOUO
         </div>
       </header>
     </div>
