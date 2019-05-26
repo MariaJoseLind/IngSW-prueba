b3856c2e90201b1c0cac8eccccfdbd0707412f85
Maria Lind <daniela.lind@globant.com>
2019-05-26 13:51:57 -0300

    commit 9

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <div>HOLA</div>
         <div>
-          ojala algun dia ande esto (HOLA MOISEEEEEE)
+          ojala algun dia ande esto (HOLA MOISeEeEeEeEeEeEe)
         </div>
       </header>
     </div>
