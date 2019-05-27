aed8055ede2e3b88d4f51a0426379ec23ca699b2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-27 02:06:12 +0000

    Deploy MariaJoseLind/IngSW-prueba to github.com/MariaJoseLind/IngSW-prueba.git:gh-pages

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>HOLA</div>
+        <div>
+          ojala algun dia ande esto (PORQUERIA 2)
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
